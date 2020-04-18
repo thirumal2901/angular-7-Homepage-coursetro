70c4885eb4cabe6da3c2a2e05cd4b515afd51ba8
thirumal2901 <thirumalm022@gmail.com>
2020-04-18 16:21:16 +0530

    4 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent],
+  imports: [ 
+    AppRoutingModule, 
+    BrowserModule, FormsModule, HttpModule 
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+//import { HttpClient } from '@angular/common/http';
+//import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'home',
+  template: `
+    <div>
+      <h1>Home</h1>
+      <h3>Total users: # {{users?.length}}</h3>
+    </div>
+  `,
+})
+export class HomeViewComponent {
+  users;
+
+  constructor(private http: Http) {
+    this.http.get('https://jsonplaceholder.typicode.com/users').map(res => res.json()).subscribe(res => this.users = res)
+  }
+}
