3bc0a57c2f2aa168ffe7920b5c31fc979770dce7
thirumal2901 <thirumalm022@gmail.com>
2020-05-01 20:29:30 +0530

    service implemented need to pass variable

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,21 +1,34 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { Router } from '@angular/router';
+import { UserPreferencesService } from './userPreferences.service';
 
 @Component({
   selector: 'login',
   templateUrl:'login.component.html'
 })
+
 export class LoginViewComponent {
 
+  // Create a private variable to hold an instance of the UserPreferencesService
+    private _userPreferencesService: UserPreferencesService;
+
    constructor(private router: Router){
+      this._userPreferencesService = new UserPreferencesService();
     }
 
-    /*public onLoginClick(){
-        alert(loginCred);
-        this.router.navigate(['./home']);
-    }*/
+  // Implement a getter to retrieve the colourPreference value
+    // from the service
+    get colour(): string {
+        return this._userPreferencesService.colourPreference;
+    }
 
+    // Implement a setter to change the colourPreference value
+    // of the service
+    set colour(value: string) {
+        this._userPreferencesService.colourPreference = value;
+    }
+    
     //public username: string;
     onSubmit() {
       if(this.username == "thiru" && this.passCred == "123" ){
