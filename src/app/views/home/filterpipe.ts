502fd4ea352e6f05f9c87008cc77b43446121606
thirumal2901 <thirumalm022@gmail.com>
2020-07-26 18:58:27 +0530

    1 file modified--filter working

--- src/app/views/home/filterpipe.ts
+++ src/app/views/home/filterpipe.ts
@@ -8,7 +8,7 @@ export class FilterPipe implements PipeTransform {
     if(!searchText) return items;
 searchText = searchText.toLowerCase();
 return items.filter( it => {
-      return it.toLowerCase().includes(searchText);
+      return it.category.toLowerCase().includes(searchText);
     });
    }
 }
